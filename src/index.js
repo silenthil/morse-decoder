f53f151502cf55b17459def248e35b7f26d63484
Evgeny <s-a-w-13@mail.ru>
2020-10-11 02:21:48 +0300

    init: start  decoder-morse

--- src/index.js
+++ src/index.js
@@ -38,9 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    if(expr.length%10!=0) return;
+    let decoder= Object.fromEntries(Object.entries(MORSE_TABLE).map(([key,value])=>[key=key.split('').map(item=>(item=='-')? 11:10 ).join(''),value]));
+    decoder= Object.fromEntries(Object.entries(decoder).map(([key,value])=>[key=func(key),value]));
+    function func(x){
+        x=x.split('')
+        while (x.length!=10){
+            x.unshift('0')
+        }
+        return x.join('');
+    }
+    let count=0;
+    let result='';
+    function d(){
+        if(count<expr.length){
+            if(expr.slice(count,count+10)=='**********'){
+            result+=' ';
+            }
+            for(let key in decoder){
+                if(expr.slice(count,count+10)==key){
+                    result+=decoder[key];
+                }
+            }
+        count+=10;
+        return d();
+        } 
+        else return result;  
+    }
+    return d()
 }
 
 module.exports = {
     decode
 }
+/*  let result="";
+    for(let i=0;i<=expr.length;i+=10){
+        if(expr.slice(i,i+10)=='**********') result+=' ';
+    }*/
